2cc0c2acf0e6558741e2e4f1559ef514f76de4fe
Tonderjar <diegolezama22@gmail.com>
2024-09-29 21:09:14 -0400

    traducir fibRoute.ts a typescript

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -4,8 +4,8 @@ import fibonacci from "./fib";
 export default (req: Request, res: Response): void => {
   const { num } = req.params;
 
-  const fibN: number = fibonacci(parseInt(num));
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  const fibN = fibonacci(parseInt(num));
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
